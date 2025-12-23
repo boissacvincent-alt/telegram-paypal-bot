7e4f2f51794b65f9155fa2e9be042b5709dc133c
boissacvincent-alt <boissac.vincent@gmail.com>
2025-12-23 13:18:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ PAYPAL_LINK = "https://paypal.me/stellaengie"
 
 PORT = int(os.environ.get("PORT", 10000))
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")  # ex: https://telegram-paypal-bot-0sf9.onrender.com
-WEBHOOK_PATH = f"/webhook/{TOKEN}"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[
@@ -25,8 +24,7 @@ def main():
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
-        webhook_url=f"{WEBHOOK_URL}{WEBHOOK_PATH}",
-        webhook_path=WEBHOOK_PATH
+        webhook_url=f"{WEBHOOK_URL}/webhook/{TOKEN}"
     )
 
 if __name__ == "__main__":
