fe48688ccd543e55652a06563821fe3512dfe12c
boissacvincent-alt <boissac.vincent@gmail.com>
2025-12-23 15:16:42 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,11 +4,9 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 
 TOKEN = os.environ.get("BOT_TOKEN")
 PAYPAL_LINK = "https://paypal.me/stellaengie"
-
 PORT = int(os.environ.get("PORT", 10000))
 WEBHOOK_URL = os.environ.get("WEBHOOK_URL")  # ex: https://telegram-paypal-bot-0sf9.onrender.com
 
-# Handler pour /start
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     keyboard = [[InlineKeyboardButton("💳 Payer 20€ via PayPal", url=PAYPAL_LINK)]]
     await update.message.reply_text(
@@ -16,18 +14,13 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
-# Handler pour loguer tous les messages entrants
 async def log_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
     print(f"Received update: {update}")
 
 def main():
     app = Application.builder().token(TOKEN).build()
-
-    # Ajout des handlers
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.ALL, log_all))  # Tout message est loggué
-
-    # Lancement du webhook
+    app.add_handler(MessageHandler(filters.ALL, log_all))
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
